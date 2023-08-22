ba8aa09df4a82af90ef1c781999ca4f0497d35a2
balvalentino <balvalentino@gmail.com>
2023-08-21 23:49:33 -0300

    upload images

---
+++ public/js/app.js
@@ -0,0 +1,28 @@
+import Dropzone from 'dropzone';
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone('#dropzone', {
+    dictDefaultMessage: 'Sube aquí tu imágen',
+    acceptedFiles: '.png,.jpg,.jpeg,.gif',
+    addRemoveLinks: true,
+    dictRemoveFile: 'Borrar Archivo',
+    maxFiles: 1,
+    uploadMultiple: false,
+})
+
+dropzone.on('sending', function (file, xhr, formData) {
+    console.log(file);
+});
+
+dropzone.on('success', function (file, response) {
+    console.log(response);
+});
+
+dropzone.on('error', function (file, response) {
+    console.log(response);
+});
+
+dropzone.on('removedfile', function (file, response){
+    console.log(response);
+});
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\ImagenController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
@@ -22,4 +25,12 @@ Route::get('/', function () {
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
+//route model binding
+Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+Route::post('posts', [PostController::class, 'store'])->name('posts.store');
+
+Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.store');
