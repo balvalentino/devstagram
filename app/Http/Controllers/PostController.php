be6d4c0d31f10e7f8f76c8e9aab00192ecebdfc5
balvalentino <balvalentino@gmail.com>
2023-08-24 00:24:49 -0300

    show de posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,8 @@ class PostController extends Controller
 
     public function index(User $user)
     {
-        $posts = Post::where('user_id', $user->id)->paginate(2);
+        //$posts = Post::where('user_id', $user->id)->paginate(20);
+        $posts = $user->posts()->paginate(20);
 
         return view('dashboard', [
             'user' => $user,
@@ -52,5 +53,12 @@ class PostController extends Controller
 
         return redirect()->route('posts.index', auth()->user()->username);
 
+    }
+
+    public function show(User $user, Post $post)
+    {
+        return view('posts.show', [
+            'post' => $post
+        ]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -38,7 +38,7 @@
             <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
                 @foreach($posts as $post)
                     <div>
-                        <a href="">
+                        <a href="{{ route('posts.show', ['user' => $user, 'post' => $post]) }}">
                             <img src="{{ asset('uploads') . '/' . $post->imagen }}"
                                  alt="Imagen del post {{ $post->titulo }}">
                         </a>
@@ -46,7 +46,7 @@
                 @endforeach
             </div>
 
-            <div>
+            <div class="my-10">
                 {{ $posts->links('pagination::tailwind') }}
             </div>
         @else
