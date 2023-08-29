453d0011111f6c5b22b8f4cbe488a39168b7c216
balvalentino <balvalentino@gmail.com>
2023-08-29 00:16:08 -0300

    likes

---
+++ resources/views/perfil/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('titulo')
+    Editar Perfil: {{ auth()->user()->username }}
+
+@endsection
+
+@section('contenido')
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\ImagenController;
 use App\Http\Controllers\LikeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\PerfilController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
@@ -30,8 +31,11 @@ Route::post('/register', [RegisterController::class, 'store']);
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
-//route model binding
-Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
+
+//Rutas para el perfil
+Route::get('/editar-perfil', [PerfilController::class, 'index'])->name('perfil.index');
+Route::post('/editar-perfil', [PerfilController::class, 'store'])->name('perfil.store');
+
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::post('posts', [PostController::class, 'store'])->name('posts.store');
 Route::get('{user:username}/posts/{post}', [PostController::class, 'show'])->name('posts.show');
@@ -43,3 +47,7 @@ Route::post('/imagenes', [ImagenController::class, 'store'])->name('imagenes.sto
 
 //Like a las fotos
 Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('posts.likes.store');
+Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
+
+//route model binding
+Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
